515bd293c4fd772b44cb40fb6f9d7311ca7473c4
Arush Kurundodi <arushkurundodi@gmail.com>
2024-02-06 16:29:15 +0530

    Update .gitignore and add new files***
    ***Add new interface and class for Producer***
    
    ***Update packages-lock.json***
    
    ***Update ProjectVersion.txt

--- Assets/MediaSoup/Producer.cs
+++ Assets/MediaSoup/Producer.cs
@@ -2,12 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.WebRTC;
+using System;
+using Mediasoup.RtpParameter;
 
-namespace Mediasoup 
+namespace Mediasoup
 {
-    public class Producer
+    interface IProducer
     {
-        
+        string _id { private get; private set; }
+        string _localId { private get; private set; }
+        bool _closed { private get; private set; }
+
+        RTCRtpSender _rtpSender { private get; private set; }
+        string _producerId { private get; private set; }
+
+        MediaStreamTrack _track { private get; private set; }
+
+        RtpParameters _rtpParameters { private get; private set; }
+
+        bool _paused { private get; private set; }
+
+        ProducerAppData _appData { private get; private set; }
+
+
+
+    }
+
+    public class Producer : EnhancedEventEmitter<ProducerEvents>, IProducer
+    {
+
+
     }
 }
-
