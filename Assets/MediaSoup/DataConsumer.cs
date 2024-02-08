c201d6898fadb46bf90c2654bb0ad32e5e42f85e
AdhikSharma <adhiksharma@gmail.com>
2024-02-08 19:33:47 +0530

    Updated DataConsumer and DataProducer
    Updated DataConsumer and DataProducer
    deleted PeerConnection.cs

--- Assets/MediaSoup/DataConsumer.cs
+++ Assets/MediaSoup/DataConsumer.cs
@@ -1,12 +1,164 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.WebRTC;
+using Mediasoup.SctpParameter;
+using Mediasoup.Internal;
 
-namespace Mediasoup 
+
+namespace Mediasoup.DataConsumers 
 {
-    public class DataConsumer
+    public interface IDataConsumer 
     {
-    
+        string id { get;}
+        string dataProducerId { get;}
+        RTCDataChannel dataChannel { get;}
+        bool isClosed { get;}
+        SctpStreamParameters sctpStreamParameters { get;}
+        object dataConsumerAppData { get;}
+        EnhancedEventEmitter observer { get;}
+
+        void Close();
+        void TransportClosed();
+    }
+
+    public class DataConsumer<TDataConsumerAppData> : EnhancedEventEmitter<ConsumerEvents>, IDataConsumer
+    {
+        public string id { get; private set; }
+        public string dataProducerId { get; private set; }
+        public RTCDataChannel dataChannel { get; private set; }
+        public bool isClosed { get; private set; }
+        public SctpStreamParameters sctpStreamParameters { get; private set; }
+        public object dataConsumerAppData { get; private set; }
+        public EnhancedEventEmitter observer { get; private set; }
+
+        public DataConsumer(string _id,string _dataProducerId, RTCDataChannel _dataChannel, SctpStreamParameters _sctpStreamParameters,
+                                TDataConsumerAppData _appData) 
+        {
+            id = _id;
+            dataProducerId = _dataProducerId;
+            dataChannel = _dataChannel;
+            sctpStreamParameters = _sctpStreamParameters;
+            if (_appData != null) dataConsumerAppData = _appData ?? typeof(TDataConsumerAppData).New<TDataConsumerAppData>()!;
+
+            observer = new EnhancedEventEmitter<ConsumerObserverEvents>();
+
+            HandleDataChannel();
+        }
+
+        ~DataConsumer() 
+        {
+            UnSubscribeEvents();
+        }
+
+        public void Close()
+        {
+            if (isClosed) return;
+
+            isClosed = true;
+            dataChannel.Close();
+
+            _ = Emit("@close");
+            _ = observer.SafeEmit("close");
+
+        }
+
+        public void TransportClosed()
+        {
+            if (isClosed) return;
+
+            isClosed = true;
+            dataChannel.Close();
+
+            _ = Emit("transportclose");
+            _ = observer.SafeEmit("close");
+        }
+
+        private void HandleDataChannel()
+        {
+            dataChannel.OnOpen += OnDataChannelOpen;
+            dataChannel.OnError += OnDataChannelError;
+            dataChannel.OnMessage += OnDataChannelMessageReceived;
+            dataChannel.OnClose += OnDatachannelClose;
+        }
+
+        private void UnSubscribeEvents()
+        {
+            dataChannel.OnOpen -= OnDataChannelOpen;
+            dataChannel.OnError -= OnDataChannelError;
+            dataChannel.OnMessage -= OnDataChannelMessageReceived;
+            dataChannel.OnClose -= OnDatachannelClose;
+        }
+
+
+        private void OnDataChannelOpen()
+        {
+            if (isClosed) return;
+            _ = SafeEmit("open");
+        }
+
+
+        private void OnDatachannelClose()
+        {
+            if (isClosed) return;
+
+            _ = Emit("@close");
+            _ = SafeEmit("close");
+            _ = observer.SafeEmit("close");
+
+        }
+
+        private void OnDataChannelMessageReceived(byte[] bytes)
+        {
+            if (isClosed) return;
+
+            _ = SafeEmit("message", bytes);
+
+        }
+
+        private void OnDataChannelError(RTCError error)
+        {
+            if (isClosed) return;
+
+            if (error.message == "sctp-failure")
+            {
+                Debug.Log($"DataChannel SCTP error {error.message} with code {error.errorType.ToString()}");
+            }
+            else 
+            {
+                Debug.Log($"DataChannel error {error.message} with code {error.errorType.ToString()}");
+            }
+
+            _ = SafeEmit("error",error);
+        }
+
+    }
+
+
+    public class DataConsumerOptions<TDataConsumerAppData> 
+    {
+        public string id;
+        public string datProducerId;
+        public SctpParameters sctpStreamParameters;
+        public string label;
+        public string protocol;
+        public TDataConsumerAppData dataConsumerAppData;
+    }
+
+    public class DataConsumerEvents 
+    {
+        public List<Action> TransportClosed { get; set; } = new List<Action>();
+        public List<Action> Open { get; set; } = new List<Action>();
+
+        public List<Action<string>> Error { get; set; } = new List<Action<string>>();
+        public List<Action> Close { get; set; } = new List<Action>();
+        public List<Action<object>> Message { get; set; } = new List<Action<object>>();
+        public List<Action> OnClose { get; set; } = new List<Action>();
+    }
+
+    public class DataConsumerObserverEvents 
+    {
+        public List<Action> Close { get; set; } = new List<Action>();
     }
 
 }
--- Assets/MediaSoup/DataProducer.cs
+++ Assets/MediaSoup/DataProducer.cs
@@ -1,12 +1,184 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.WebRTC;
+using Mediasoup.SctpParameter;
+using Mediasoup.Internal;
 
-namespace Mediasoup 
+namespace Mediasoup.DataProducers 
 {
-    public class DataProducer
+    public interface IDataProducer 
     {
+        string id { get; }
+        RTCDataChannel dataChannel { get; }
+        bool isClosed { get; }
+        SctpStreamParameters sctpStreamParameters { get; }
+        object dataProducerAppData { get; }
+        EnhancedEventEmitter observer { get; }
+
+        void Close();
+        void TransportClosed();
+        void Send(object message);
+
+    }
+
+    public class DataProducer<TDataProducerAppData> : EnhancedEventEmitter<DataProducerEvents>, IDataProducer
+    {
+        public string id { get; private set; }
+
+        public RTCDataChannel dataChannel { get; private set; }
+
+        public bool isClosed { get; private set; }
+
+        public SctpStreamParameters sctpStreamParameters { get; private set; }
+
+        public object dataProducerAppData { get; private set; }
+
+        public EnhancedEventEmitter observer { get; private set; }
+
+
+        public DataProducer(string _id, RTCDataChannel _dataChannel, SctpStreamParameters _sctpStreamParameters,
+                                TDataProducerAppData _appData)
+        {
+            id = _id;
+            dataChannel = _dataChannel;
+            sctpStreamParameters = _sctpStreamParameters;
+            if (_appData != null) dataProducerAppData = _appData ?? typeof(TDataProducerAppData).New<TDataProducerAppData>()!;
+
+            observer = new EnhancedEventEmitter<ConsumerObserverEvents>();
+
+            HandleDataChannel();
+        }
+
+        ~DataProducer()
+        {
+            UnSubscribeEvents();
+        }
+
+
+        public void Close()
+        {
+            if (isClosed) return;
+
+            isClosed = true;
+            dataChannel.Close();
+
+            _ = Emit("@close");
+            _ = observer.SafeEmit("close");
+        }
+
+        public void TransportClosed()
+        {
+            if (isClosed) return;
+
+            isClosed = true;
+            dataChannel.Close();
+
+            _ = Emit("transportclose");
+            _ = observer.SafeEmit("close");
+        }
+
+        public void Send(object message)
+        {
+            if (isClosed) 
+            {
+                throw new InvalidOperationException("closed");
+            }
+
+            if (message is string)
+            {
+                string msg = (string)message;
+                dataChannel.Send(msg);
+            } else if (message is byte[]) 
+            {
+                byte[] msg = (byte[])message;
+                dataChannel.Send(msg);
+            }
+        }
+
+
+        private void HandleDataChannel()
+        {
+            dataChannel.OnOpen += OnDataChannelOpen;
+            dataChannel.OnError += OnDataChannelError;
+            dataChannel.OnMessage += OnDataChannelMessageReceived;
+            dataChannel.OnClose += OnDatachannelClose;
+        }
+
+        private void UnSubscribeEvents()
+        {
+            dataChannel.OnOpen -= OnDataChannelOpen;
+            dataChannel.OnError -= OnDataChannelError;
+            dataChannel.OnMessage -= OnDataChannelMessageReceived;
+            dataChannel.OnClose -= OnDatachannelClose;
+        }
+
+
+        private void OnDataChannelOpen()
+        {
+            if (isClosed) return;
+            _ = SafeEmit("open");
+        }
+
+
+        private void OnDatachannelClose()
+        {
+            if (isClosed) return;
+
+            _ = Emit("@close");
+            _ = SafeEmit("close");
+            _ = observer.SafeEmit("close");
+
+        }
+
+        private void OnDataChannelMessageReceived(byte[] bytes)
+        {
+            if (isClosed) return;
+        }
+
+        private void OnDataChannelError(RTCError error)
+        {
+            if (isClosed) return;
+
+            if (error.message == "sctp-failure")
+            {
+                Debug.Log($"DataChannel SCTP error {error.message} with code {error.errorType.ToString()}");
+            }
+            else
+            {
+                Debug.Log($"DataChannel error {error.message} with code {error.errorType.ToString()}");
+            }
+
+            _ = SafeEmit("error", error);
+        }
+
         
+    }
+
+    public class DataProducerOptions<TDataProducerAppData>
+    {
+        public bool ordered;
+        public int maxPacketLifeTime;
+        public int maxRetransmits;
+        public string label;
+        public string protocol;
+        public TDataProducerAppData dataConsumerAppData;
+    }
+
+    public class DataProducerEvents
+    {
+        public List<Action> TransportClosed { get; set; } = new List<Action>();
+        public List<Action> Open { get; set; } = new List<Action>();
+
+        public List<Action<string>> Error { get; set; } = new List<Action<string>>();
+        public List<Action> Close { get; set; } = new List<Action>();
+        public List<Action<object>> Message { get; set; } = new List<Action<object>>();
+        public List<Action> OnClose { get; set; } = new List<Action>();
+    }
+
+    public class DataProducerObserverEvents
+    {
+        public List<Action> Close { get; set; } = new List<Action>();
     }
 }
 
